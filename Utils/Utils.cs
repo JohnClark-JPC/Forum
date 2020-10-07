05f7fc3dd8299480f5ef5afcf430769de60d61a3
John Clark <john.clark012@outlook.com>
2020-10-07 12:00:37 -0400

    1200 Wednesday

---
+++ Utils/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Forum.Models;
+
+namespace Forum.Utils
+{
+    public class Utils
+    {
+        int ForumChoice = 0;
+        int TopicChoice = 0;
+        int PostChoice = 0;
+
+        //public void SetForum(int forumChoice) => ViewBag.ForumChoice = forumChoice;
+    }
+}
